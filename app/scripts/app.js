6f5b6effc8b9cb95c10219759749fe1e3a12b76d
Alberto <alberto.grau.margarida@gmail.com>
2016-11-29 18:20:20 -0500

    added  and

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,25 @@
 // angular.module('blocJams', ['ui.router']);
 (function() {
   function config($stateProvider, $locationProvider) {
+// $stateProvider creates a new application state, we will need 4: name, URL route, controller, and template.
+// $locationProvider configures apps deep linking paths
+
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+// $stateProvider.state(stateName, stateConfig)
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+      });
   }
 
   angular
