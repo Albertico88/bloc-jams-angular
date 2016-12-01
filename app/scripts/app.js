c58223d6245b23b7f7b95ff73b11fa68745482d4
Alberto <alberto.grau.margarida@gmail.com>
2016-12-01 18:05:07 -0500

    set up routes and states

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,16 @@
 (function() {
   function config($stateProvider, $locationProvider) {
-// $stateProvider creates a new application state, we will need 4: name, URL route, controller, and template.
-// $locationProvider configures apps deep linking paths
-
     $locationProvider // configures app paths
       .html5Mode({
         enabled: true,  // Disables Hashbang #! prefixes in url
-        requireBase: false // setting this to false fixes $location error
+        requireBase: false  // setting this to false fixes $location error
       });
 
     $stateProvider
-    // .state(stateName, stateConfig)
       .state('landing', {
-        url: '/',
-        templateUrl: '/templates/landing.html'
-      })
+          url: '/', // address bar url name 
+          templateUrl: '/templates/landing.html'
+         })
       .state('collection', {
         url: '/collection',
         templateUrl: '/templates/collection.html'
