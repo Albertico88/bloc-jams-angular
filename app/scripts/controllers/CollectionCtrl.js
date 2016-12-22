bbb39807656275c7d502949c359f6b7228d46f46
Alberto <alberto.grau.margarida@gmail.com>
2016-12-22 18:21:19 -0400

    completed assignment: added services to CollectionCtrl

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,27 +1,9 @@
 (function() {
-  function CollectionCtrl() {
-    this.albums = [];
-    for (var i=0; i < 12; i++) {
-      this.albums.push(angular.copy(albumPicasso));
-    }
+  function CollectionCtrl(Fixtures) {
+    this.albums = Fixtures.getCollection(12);
   }
 
   angular
     .module('blocJams')
-    .controller('CollectionCtrl', CollectionCtrl);
+    .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
 })();
-
-var albumPicasso = {
-  title: 'Colors',
-  artist: 'Pablo Picasso',
-  lable: 'Cubism Records',
-  year: '1881',
-  albumArtUrl: '/assets/images/17.png',
-  songs: [
-    { title: 'Blue', duration: 161.71, audioURl: 'assets/music/blue' },
-    { title: 'Green', duration: 103.96, audioURl: 'assets/music/green' },
-    { title: 'Red', duration: 268.45, audioURl: 'assets/music/red' },
-    { title: 'Pink', duration: 153.14, audioURl: 'assets/music/pink' },
-    { title: 'Magenta', duration: 374.22, audioURl: 'assets/music/magenta' }
-  ]
-};
