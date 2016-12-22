beb078f94751eac879c97d41f6dc77cda9899835
Alberto <alberto.grau.margarida@gmail.com>
2016-12-22 14:11:05 -0400

    added services (factory) for Fixtures

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,22 +1,9 @@
 (function() {
-  function AlbumCtrl() {
-    this.albumData = {
-      title: 'Colors',
-      artist: 'Pablo Picasso',
-      lable: 'Cubism Records',
-      year: '1881',
-      albumArtUrl: '/assets/images/17.png',
-      songs: [
-        { title: 'Blue', duration: 161.71, audioURl: 'assets/music/blue' },
-        { title: 'Green', duration: 103.96, audioURl: 'assets/music/green' },
-        { title: 'Red', duration: 268.45, audioURl: 'assets/music/red' },
-        { title: 'Pink', duration: 153.14, audioURl: 'assets/music/pink' },
-        { title: 'Magenta', duration: 374.22, audioURl: 'assets/music/magenta' }
-      ]
-    };
+  function AlbumCtrl(Fixtures) {
+    this.albumData = Fixtures.getAlbum();
   }
 
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', AlbumCtrl);
+    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
