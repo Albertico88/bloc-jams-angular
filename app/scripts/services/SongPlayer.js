c8c4d0d4fb1bd89332ebd7ad9d65f5327ef1bf42
Alberto <alberto.grau.margarida@gmail.com>
2017-01-20 18:11:07 -0500

    Completed checkpoint 7: Refactored SongPlayer and added a setSong function

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,23 +3,37 @@
     var SongPlayer = {};
 
     var currentSong = null;
+    
+/**
+* @desc Buzz Object audio file
+* @type { Object }
+*/
     var currentBuzzObject = null;
 
-// PLAY Method
+/**
+* SETSONG Function (private)
+* @function setSong
+* @desc Stops currently playing song and loads new audio files as currentBuzzObject
+* @param { Object } song
+*/
+    var setSong = function(song) {
+      if (currentBuzzObject) {
+        currentBuzzObject.stop();
+        currentSong.playing = null;
+      }
+
+      currentBuzzObject = new buzz.sound(song.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+      });
+
+      currentSong = song;
+    };
+
+// PLAY Method (public)
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
-        if (currentBuzzObject) {
-          currentBuzzObject.stop();
-          currentSong.playing = null; // 'playing' variable TBD
-        }
-
-        currentBuzzObject = new buzz.sound(song.audioUrl, {
-          formats: ['mp3'],
-          preload: true
-        });
-
-        currentSong = song;
-
+        setSong(song);
         currentBuzzObject.play();
         song.playing = true;
 
@@ -30,7 +44,7 @@
       }
     };
 
-// PAUSE Method
+// PAUSE Method (public)
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
